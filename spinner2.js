791873448a8eb825cee0c3c9d53737aec4cbccc6
Aprajit Kumar <aprajit.k@gmail.com>
2021-08-11 11:58:20 +0000

    fixed spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,6 +1,6 @@
 process.stdout.write('hello from spinner2.js... \rheyyy\n');
 let delay = 100;
-let spinners = ['\r|  ', '\r/  ', '\r-  ','\r\\  ']
+let spinners = ['\r|  ', '\r/  ', '\r-  ','\r\\  ', '\r\|   ', '\r\/   ', '\r-   ', '\r\\  '];
 for (const r of spinners) {
     setTimeout(() =>{
       process.stdout.write(r);
